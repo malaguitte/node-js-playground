b57ec6d7c4bc76ff3f700b6a88093e95ece4c6f8
Anderson Malagutti <upmalagutti@gmail.com>
2017-09-20 00:51:01 -0300

    module.exports vs exports

---
+++ D12_ExportsVsModuleExports/app.js
@@ -0,0 +1,9 @@
+const greet = require('./greet');
+// \/ Throws an error
+// greet.greet();
+const greet2 = require('./greet2');
+// \/ Works
+greet2.greet();
+
+
+//JUST USE module.exports - It is easier and simpler. :)
