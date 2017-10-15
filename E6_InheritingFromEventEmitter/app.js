61730a6e1cbe17355bc2952956bb1111f4421d06
Anderson Malagutti <upmalagutti@gmail.com>
2017-10-15 23:48:21 +0200

    Added example of how to use 'utils.inherits'

---
+++ E6_InheritingFromEventEmitter/app.js
@@ -0,0 +1,23 @@
+var EventEmitter = require('events');
+var util = require('util');
+
+function Greetr()
+{
+  this.greeting = 'Hello World';
+}
+
+util.inherits(Greetr, EventEmitter);
+
+Greetr.prototype.greet = function(data)
+{
+  console.log(this.greeting + ' : ' + data);
+  this.emit('greet', data);
+}
+
+var greeter1 = new Greetr();
+
+greeter1.on('greet', function(data) {
+  console.log('Someone has been greeted : ' + data);
+});
+
+greeter1.greet('Tony');
