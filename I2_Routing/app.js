482657f1fa8e3aeb894120c1d95b22dab74726cd
Anderson Malagutti <upmalagutti@gmail.com>
2018-08-23 20:48:13 +0200

    passing a param to an url using express, retrieving the param from the request

---
+++ I2_Routing/app.js
@@ -0,0 +1,29 @@
+/**
+ * Run `npm install` first. ;)
+ */
+
+//using express module
+var express = require('express');
+var app = express();
+
+//tries to get the environment variable PORT, defaults to 3000.
+var port =  process.env.PORT || 3000;
+
+app.get('/', function(request, response) {
+  //express is setting the contenty-type automatically here
+  response.send('<html><head></head><body><h1>Hello World!</h1></body></html>');
+});
+
+//generic url, "id" can be anything
+app.get('/person/:id', function(request, response) {
+	let personId = request.params.id;
+  //express is setting the contenty-type automatically here
+  response.send('<html><head></head><body><h1>Person: '+ personId +' </h1></body></html>');
+});
+
+app.get('/api', function(request, response) {
+  //express is setting the contenty-type automatically here
+  response.json({ firstname: 'Anderson', surname: 'Malagutti' });
+});
+
+app.listen(port);
